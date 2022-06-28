1f8898fe71a5793bad6b0f6624b9870049f06324
Pedro <pedrosteinmuller10105@hotmail.com>
2022-06-28 14:02:38 -0300

    resolucao requisito 9

--- src/challenges.js
+++ src/challenges.js
@@ -81,13 +81,19 @@ function fizzBuzz(entrada) {
 }
 
 // Desafio 9
-function encode() {
-  // seu código aqui
-}
-function decode() {
-  // seu código aqui
+function encode(enc) {
+  for (let index = 0; index < enc.length; index += 1) {
+    enc = enc.replace('a', 1).replace('e', 2).replace('i', 3).replace('o', 4).replace('u', 5)
+  }
+  return enc;
 }
 
+function decode(enc1) {
+  for (let index = 0; index < enc1.length; index += 1) {
+    enc1 = enc1.replace('1', 'a').replace('2', 'e').replace('3', 'i').replace('4', 'o').replace('5', 'u')
+}
+return enc1;
+}
 // Desafio 10
 function techList() {
   // seu código aqui
